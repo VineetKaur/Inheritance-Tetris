81d71a80cbaa904a2ad68bcfc90b2ad7523490b3
SnoviyaD <30373607+SnoviyaD@users.noreply.github.com>
2017-07-23 09:36:29 +0530

    Add files via upload

---
+++ Sound.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import sun.audio.*;
+import java.io.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Sound
+{
+
+    /*public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(200,200);
+        JButton button = new JButton("click here");
+        frame.add(button);
+        button.addActionListener(new AL());
+        frame.show(true);
+    }
+    public static class AL implements ActionListener
+    {
+        public final void actionPerformed(ActionEvent e)
+        {
+            music();
+        }
+
+    }*/
+
+   
+    public static void music()
+    {
+        AudioPlayer MGP = AudioPlayer.player;
+        AudioStream BGM;
+        AudioData MD;
+        ContinuousAudioDataStream loop = null;
+       
+        try{
+            BGM =new AudioStream(new FileInputStream("Smashing-Yuri_Santana-1233262689.wav"));
+            MD = BGM.getData();
+            loop = new ContinuousAudioDataStream    (MD);
+        }catch(IOException error){
+           
+        }
+        MGP.start(loop);
+       
+
+    }
+}
